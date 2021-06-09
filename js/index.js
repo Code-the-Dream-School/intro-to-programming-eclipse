c2300e29360dba09d15f78395882853d637cf10e
PerryL17 <77135000+PerryL17@users.noreply.github.com>
2021-06-09 14:48:16 -0700

    added messageSection

--- js/index.js
+++ js/index.js
@@ -8,16 +8,44 @@ const skills = ['highly organized', 'bilingual English/Spanish', 'self motivated
 const skillsSection = document.getElementById('skills');
 const skillsList = skillsSection.getElementsByTagName('ul');
 
-
-
-
-
 for(let i = 0; i < skills.length; i++) {
   let skill = document.createElement('li');
   skill.innerHTML = skills[i];
   skillsSection.appendChild(skill);
 }
+  let messageForm = document.getElementsByName("leave_message")[0];
+  let messageSection = document.getElementById("messages");
+  
+  messageForm.addEventListener('submit',(e) => {
+    e.preventDefault();
+    let name = e.target.name.value;
+    let email = e.target.email.value;
+    let message = e.target.message.value;
 
+    console.log(`Name: ${name} Email: ${email} Message: ${message}`);
+      
+    
+    let messageList = messageSection.getElementsByTagName("ul")[0];
+    let newMessage = document.createElement("li");
+      
+    newMessage.innerHTML= `<a href= "mailto: ${email}">${name}</a><span>:${message}</span>`;
+    messageList.appendChild(newMessage);
+      
+    messageForm.reset();
+    
+      const removeButton = document.createElement("button");
+      removeButton.textContent= "remove";
+      removeButton.type= "button";
+      //messageSection.appendChild(removeButton);
+      newMessage.appendChild(removeButton);
+      messageList.appendChild(newMessage);
+      
+      removeButton.addEventListener('click', (e) => {
+      let entryToRemove = e.target.parentElement;
+      entryToRemove.remove();
+      
+     
+      
 
-
-
+      });
+  });
