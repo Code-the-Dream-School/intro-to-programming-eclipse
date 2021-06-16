fcd90d08a15d1949938d6b13d0244e2f1a1a9c49
mnca16 <mnca.delgado@gmail.com>
2021-06-15 21:45:03 -1100

    adding css file and styling portfolio

--- instructions/js/index.js
+++ instructions/js/index.js
@@ -1,35 +1,30 @@
-
+//This event let my code appear after the page is loaded 
 window.addEventListener('load', (event) => {
+
+// footer of my portfolio (name and year)
 const today = new Date()
-
 const thisYear = today.getFullYear()
-
 const footer = document.querySelector('footer')
-
 const copyright = document.createElement('p')
-
 copyright.innerHTML = `Monica ${thisYear} &copy;`
-
 footer.appendChild(copyright)
 
+
+// skills section
 const skills = ['JS', 'CSS', 'HTML']
-
 const skillsSection = document.getElementById('skills')
-
 const skillList = skillsSection.querySelector('ul')
-
+// adding and creating the list of skills
 for(i=0; i < skills.length; i++){
 const skill = document.createElement('li')
 skill.innerHTML = skills[i]
 skillList.appendChild(skill)
-
 }
 
+
+
 //form code
-
 const messageForm = document.getElementsByName('leave_message')[0]
-
-
 messageForm.addEventListener('submit', (e) => {
 
    e.preventDefault()
@@ -42,14 +37,14 @@ messageForm.addEventListener('submit', (e) => {
    const messageSection = document.getElementById('messages')
    const messageList = messageSection.querySelector('ul')
    const newMessage = document.createElement ('li') 
-   newMessage.innerHTML = `<a href ="mailto: ${email} "> wrote: ${name}</a><span>${message}</span>`
+   newMessage.innerHTML = `<a href ="mailto: ${email} "> ${name}</a><span>${message}</span>`
    
    //Second button (remove button)
    const removeButton = document.createElement('button')
    removeButton.innerText = 'Remove'
    removeButton.type = 'button'
 
-   removeButton.addEventListener('onclick', (e) => {
+   removeButton.addEventListener('click', (e) => {
        const entry = e.target.parentNode
        entry.remove() 
    })
