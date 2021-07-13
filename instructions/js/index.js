3648917367cbfdcb685859d4532efd877d34ac8a
mnca16 <mnca.delgado@gmail.com>
2021-07-12 21:27:49 -1100

    working with fetch

--- instructions/js/index.js
+++ instructions/js/index.js
@@ -1,31 +1,31 @@
-//This event let my code appear after the page is loaded 
+//This event let my code appear after the page (HTML) is loaded 
 window.addEventListener('load', (event) => {
 
-// footer of my portfolio (name and year)
-const today = new Date()
-const thisYear = today.getFullYear()
-const footer = document.querySelector('footer')
-const copyright = document.createElement('p')
-copyright.innerHTML = `Monica ${thisYear} &copy;`
-footer.appendChild(copyright)
+  // footer of my portfolio (name and year)
+  const today = new Date()
+  const thisYear = today.getFullYear()
+  const footer = document.querySelector('footer')
+  const copyright = document.createElement('p')
+  copyright.innerHTML = `Monica ${thisYear} &copy;`
+  footer.appendChild(copyright)
 
 
-// skills section
-const skills = ['JS', 'CSS', 'HTML']
-const skillsSection = document.getElementById('skills')
-const skillList = skillsSection.querySelector('ul')
-// adding and creating the list of skills
-for(i=0; i < skills.length; i++){
-const skill = document.createElement('li')
-skill.innerHTML = skills[i]
-skillList.appendChild(skill)
-}
+  // skills section
+  const skills = ['JS', 'CSS', 'HTML']
+  const skillsSection = document.getElementById('skills')
+  const skillList = skillsSection.querySelector('ul')
+  // adding and creating the list of skills
+  for(i=0; i < skills.length; i++){
+  const skill = document.createElement('li')
+  skill.innerHTML = skills[i]
+  skillList.appendChild(skill)
+  }
 
 
 
-//form code
-const messageForm = document.getElementsByName('leave_message')[0]
-messageForm.addEventListener('submit', (e) => {
+  //form code
+  const messageForm = document.getElementsByName('leave_message')[0]
+  messageForm.addEventListener('submit', (e) => {
 
    e.preventDefault()
 
@@ -56,28 +56,24 @@ messageForm.addEventListener('submit', (e) => {
 
   })
 
+   fetch('https://api.github.com/users/mnca16/repos')
+   .then(response => response.json())
+   .then(data => repository(data))
+   .catch(error => console.log('The promise failed', error));
 
-//AJAX practice... adding GitHub repositories to my portfolio
-const githubRequest = new XMLHttpRequest();
-//Open method (as the assigment asks you to do)
-githubRequest.open('GET','https://api.github.com/users/mnca16/repos');
-githubRequest.send();
-githubRequest.addEventListener('load', (event) => {
-const repositories = JSON.parse(githubRequest.response)
-console.log(repositories);
-let projectSection = document.getElementById('projects');
-let projectList = projectSection.querySelector('ul');
-for(let i = 0; i < repositories.length; i++) {
-    let project = document.createElement('li');
-    project.innerHTML = `<a href="${repositories[i].html_url}"> ${repositories[i].name}</a> ${repositories[i].description} ${repositories[i].created_at}}`
-    projectList.appendChild(project);
+   function repository (data) {
+      let projectSection = document.getElementById('projects');
+      let projectList = projectSection.querySelector('ul');
 
-}
-})
+      for (let i = 0; i < data.length; i++){
+         let project = document.createElement('li');
+         project.innerHTML = `<a href="${data[i].html_url}">${data[i].name}</a>`
+         projectList.appendChild(project);
+     }
+    }
+  
  
-
-    
-})
+  })
 
 
  
